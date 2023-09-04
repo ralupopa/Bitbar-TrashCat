4a994009c958ab86c19ec5bb5be691eba148743e
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-09-04 18:18:44 +0300

    add setup for server side with AltServer setup on VM windows

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Bitbar-TrashCat
+
+## Server-Side
+
+Follow [Running Cloud-Side Appium tests](https://support.smartbear.com/bitbar/docs/en/mobile-app-tests/automated-testing/appium-support/running-cloud-side-appium-tests.html) to prepare zip archive.
+
+1. Upload zip with all tests and with `run-test.sh` script to launch test execution at the root level of the package.
+2. Upload .apk / .ipa
+3. Create new test run with previously uploaded files
+4. If you use trial devices provided as group, please leave just one running and stop all the others. At the moment AltServer does not allow running same tests on same app simultaneously.
+
+Tested setup using AltServer running in a Windows Azure VM. The IP of the VM needs to be specified in `BaseTest.cs` when altDriver is instantiated. Additionally the game build needs to be instrumented with the same host IP.
+
+## Client-Side
+
+
---
+++ tests/BaseTest.cs
@@ -0,0 +1,41 @@
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace alttrashcat_tests_csharp.tests
+{
+    public class BaseTest
+    {
+        public AndroidDriver<AndroidElement> appiumDriver;
+        public AltDriver altDriver;
+        [OneTimeSetUp]
+        public void SetupAppium()
+        {
+
+            AppiumOptions capabilities = new AppiumOptions();
+
+            capabilities.AddAdditionalCapability("appium:deviceName", "Android");
+            capabilities.AddAdditionalCapability("platformName", "Android");
+            capabilities.AddAdditionalCapability("automationName", "UIAutomator2");
+            capabilities.AddAdditionalCapability("newCommandTimeout", 2000);
+
+            // See available devices at: https://cloud.bitbar.com/#public/devices
+            capabilities.AddAdditionalCapability("app", "application.apk");
+
+            Console.WriteLine("WebDriver request initiated. Waiting for response, this typically takes 2-3 mins");
+
+            appiumDriver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(36000));
+            appiumDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Console.WriteLine("Appium driver started");
+            altDriver = new AltDriver(host: "insert_ip_here");
+            Console.WriteLine("AltDriver started");
+        }
+
+        [OneTimeTearDown]
+        public void DisposeAppium()
+        {
+            Console.WriteLine("Ending");
+            altDriver.Stop();
+            appiumDriver.Quit();
+        }
+    }
+}
